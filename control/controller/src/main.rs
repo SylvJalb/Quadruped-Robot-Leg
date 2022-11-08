1af99a318ce21583a32931451cdd2e95601bbe04
Sylvain JALBERT <sylv.jalbert@gmail.com>
2022-11-08 21:18:45 +0100

    test the rust code with a simple loop

--- control/controller/src/main.rs
+++ control/controller/src/main.rs
@@ -3,7 +3,16 @@ use nalgebra::Vector3;
 
 fn main() {
     // create a new Leg instance
-    let mut leg = leg::Leg::new(Vector3::new(120 as f32, 50 as f32, -425 as f32));
-
+    let mut leg = leg::Leg::new(Vector3::new(120.0, 50.0, -425.0));
     println!("{:?}", leg);
+
+    // RUN A SIMPLE TEST
+    // loop from -100 to 50 with a step of 10
+    for i in (-100..51).step_by(10) {
+        // move the leg
+        match leg.set_foot_position(&Vector3::new(120.0, i as f32, -425.0)) {
+            Ok(_) => println!("{:?}", leg),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 }
